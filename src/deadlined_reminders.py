fd2fda920ace50a18d89a8d6e8a9d35366202a45
Wendy Shaffer <wshaffer@vmware.com>
2021-02-11 11:02:30 -0800

    Fixing typo

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from collections.abc import Iterable
 
-class DeadlineMetaReminder(Iterable, metaclass=ABCMeta):
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
     def is_due():
         pass
