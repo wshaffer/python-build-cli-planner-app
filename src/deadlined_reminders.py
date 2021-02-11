aa92d9a22fee41e1254c0f41c47f00a39b3b069c
Wendy Shaffer <wshaffer@vmware.com>
2021-02-11 11:09:32 -0800

    Alternative way of implementing Abstract Base Class

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,7 +1,12 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DeadlinedReminder(ABC, Iterable):
     @abstractmethod
     def is_due(self):
         pass
