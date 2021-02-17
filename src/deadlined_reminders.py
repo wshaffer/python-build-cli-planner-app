236a89e817304605697241bfe2ce7aeca5814f7a
Wendy Shaffer <wshaffer@vmware.com>
2021-02-16 16:26:41 -0800

    Implementing is_due()

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
 from dateutil.parser import parse
+from datetime import datetime
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
@@ -16,4 +17,6 @@ class DateReminder(DeadlinedReminder):
     def __init__(self, text, date)
         self.date = parse(date, dayfirst=True)
         self.text = text
-        
+    def is_due(self)
+        if self.date <= datetime.now():
+            return True
